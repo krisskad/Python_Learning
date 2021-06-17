275ea4798d58a01217fb03d0b2d59fee08597005
krisskad <krishna.g.kadam98500@gmail.com>
2021-06-17 19:31:55 +0530

    fibonacci

--- Basic Programs/fibonacci.py
+++ Basic Programs/fibonacci.py
@@ -1,3 +1,14 @@
 # print fibonacci sequence till given number
+# 0, 1, 1, 2, 3, 5, 8, 13,
 
-N = int(input().split())
+N = int(input())
+
+
+feb = [0,1]
+for i in range(1, N+1):
+    temp = feb[i-1] + feb[i]
+    if temp>N:
+        break
+    feb.append(temp)
+
+print(feb)
